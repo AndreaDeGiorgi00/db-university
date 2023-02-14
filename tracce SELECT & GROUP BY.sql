80d69881ffcd21b9de399d6eb2e535191baa0f27
AndreaDeGiorgi00 <andrea.adedegiorgi@gmail.com>
2023-02-14 17:00:15 +0100

    esercizio 7 completato

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -20,7 +20,7 @@ SELECT `hour` , `location` , `date` FROM `exams` WHERE `hour` > '14:00:00' AND `
 
 SELECT `level`, `name` FROM `degrees` WHERE `level` = 'magistrale';
 -- 7. Da quanti dipartimenti è composta l'università? (12)
-
+SELECT COUNT(`name`) FROM `departments`;
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
