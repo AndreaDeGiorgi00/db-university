4e3e9ae1f7daa5e1f789d6ca756334a18db9e6dd
AndreaDeGiorgi00 <andrea.adedegiorgi@gmail.com>
2023-02-14 17:01:35 +0100

    esercizio 8 completato

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -24,7 +24,7 @@ SELECT COUNT(`name`) FROM `departments`;
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
-
+SELECT `name` , `phone` FROM `teachers` WHERE `phone` IS NULL;
 
 -- ------------------------------------------ QUERY CON GROUP BY
 
